9297adcc54a7e2b93c15fb644fc654ecd3908d97
tsbarbre <tsbarbre@yahoo.com>
2020-11-27 22:06:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Data Types
+# String
+print("Hello"[4])
+print("123" + "456")
+#above code just puts 123345 together
+
+#Integer 
+print(123 + 456)
+
+#Float when using decimals
+print(12.3 + 45.6)
+
+#Boolean is true or false
+
